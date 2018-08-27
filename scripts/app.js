1fdb377589c61f8e6925768a6e754cccd05c3581
Kent Tokunaga <kenttokunaga@gmail.com>
2018-08-27 14:35:10 -0400

    start modularizing tic tac toe

---
+++ scripts/app.js
@@ -0,0 +1,25 @@
+// eslint-disable-next-line no-unused-vars
+const state = {
+  board: Array(9).fill(null),
+  xIsNext: true
+};
+
+// State modification functions
+
+// Render functions
+
+// Event Listeners
+$(document).ready(function() {
+  // game.bindEventListeners();
+  // game.render();
+});
+
+
+// [0, 1, 2],
+// [3, 4, 5],
+// [6, 7, 8],
+// [0, 3, 6],
+// [1, 4, 7],
+// [2, 5, 6],
+// [0, 4, 8],
+// [2, 4, 6]
