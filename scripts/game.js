25829355e509dfcea7aa20ca091e1234d60679eb
Kent Tokunaga <kenttokunaga@gmail.com>
2018-08-27 15:20:00 -0400

    working on implementing the board

--- scripts/game.js
+++ scripts/game.js
@@ -4,16 +4,33 @@
 const game = (function(){
   const board = $('.board');
 
-  const generateCell = function(cell, cellIndex) {
+  const generateCellElement = function(cellIndex) {
     return `
       <div class="cell" id="${cellIndex}">
-        <p>${cell}</p>
+        <p>${cellIndex}</p>
       </div>
     `;
   };
 
+  };
+
+  const generateBoardElement = function() {
+    let board = '';
+
+    for (let i=0; i<9; i++) {
+      if (i % 3 === 0) board += '<div class="row">';
+      board += generateCellElement(i);
+      if (i % 3 === 0) board += '</div>';
+    }
+
+    return board;
+  };
+
   const render = function() {
-    
+    const board = generateBoardElement();
+  
+    // insert that HTML into the DOM
+    $('.board').html(board);
   };
   
   return {
